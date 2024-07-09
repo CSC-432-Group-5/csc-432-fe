10c257fb2dfd31d90acf06a19fd4350d4a086cc4
Musa Jubril <jewbreel1@gmail.com>
2024-07-09 23:07:59 +0100

    added Mt library

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "@material-tailwind/react";
+import { Montserrat } from "next/font/google";
 
+const monts = Montserrat({ subsets: ["latin"] });
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider>
+      <div className={monts.className}>
+        <Component {...pageProps} />
+      </div>
+    </ThemeProvider>
+  );
 }
